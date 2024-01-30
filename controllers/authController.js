26b43771a79cb49dd25f208bba5bafb7ce3f6c04
ninokvakhadze <ninokvakhadze2@gmail.com>
2024-01-31 01:17:30 +0400

    fixed email.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,18 +13,17 @@ const signToken = (id) => {
   });
 };
 
-const createSendToken = (user, statusCode, res)=>{
-   const token = signToken(user._id)
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
 
-   res.status(statusCode).json({
+  res.status(statusCode).json({
     status: "success",
     token,
     data: {
       user: user,
     },
   });
-
-}
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -33,7 +32,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  createSendToken(newUser, 201, res)
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -52,7 +51,7 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 
   //3) if everything ok, send to user
-  createSendToken(user, 200, res)
+  createSendToken(user, 200, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -139,14 +138,15 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   // 1) Get user based on the token
   const hashedToken = crypto
     .createHash("sha256")
-    .update(req.params.token.digest("hex"));
+    .update(req.params.token)
+    .digest("hex");
 
   const user = await User.findOne({
     passwordResetToken: hashedToken,
     passwordResetExpires: { $gt: Date.now() },
   });
-
-  //2) If toke has not expired, and there is user, set the new password
+  console.log(user)
+  // 2) If token has not expired, and there is user, set the new password
   if (!user) {
     return next(new AppError("Token is invalid or has expired", 400));
   }
@@ -154,25 +154,28 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   user.passwordConfirm = req.body.passwordConfirm;
   user.passwordResetToken = undefined;
   user.passwordResetExpires = undefined;
-
   await user.save();
-  // 3) Upadte changedPasswordAt property for the user
-  // 4) Log the user in, sent JWT
-  createSendToken(user, 200, res)
+
+  // 3) Update changedPasswordAt property for the user
+  // 4) Log the user in, send JWT
+  createSendToken(user, 200, res);
 });
 
+exports.updatePassword = catchAsync(async (req, res, next) => {
+   // 1) Get user from collection
+   const user = await User.findById(req.user.id).select('+password');
 
-exports.updatePassword = catchAsync(async(req, res, next) => {
-  // 1) get user 
-    const user = await User.findById(req.user.id).select('+password')
-  // 2) if password is correct
-  if(!(user.correctPassword(req.body.passwordConfirm, user.password))){
-    return next(new AppError('Your current password is wrong', 401))
-  }
-  // 3) If so, update password
-   user.password = req.body.password
-   user.passwordConfirm = req.body.passwordConfirm
-   await user.save()
-  // 4) log user in, send  jwt 
-  createSendToken(user, 200, res);
-})
+   // 2) Check if POSTed current password is correct
+   if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+     return next(new AppError('Your current password is wrong.', 401));
+   }
+ 
+   // 3) If so, update password
+   user.password = req.body.password;
+   user.passwordConfirm = req.body.passwordConfirm;
+   await user.save();
+   // User.findByIdAndUpdate will NOT work as intended!
+ 
+   // 4) Log user in, send JWT
+   createSendToken(user, 200, res);
+});
--- utils/email.js
+++ utils/email.js
@@ -3,12 +3,13 @@ const nodemailer = require('nodemailer');
 const sendEmail = async options => {
   // 1) Create a transporter
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    service: "Gmail",
+    logger: true,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD
     }
+
   });
 
   // 2) Define the email options
